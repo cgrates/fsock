79ca2b29d08bacb90c5a71376984d03c250c9b1e
Trial97 <alexandru.tripon@itsyscom.com>
2019-01-13 17:42:13 +0200

    Added integration tests

---
+++ fsock_it_test.go
@@ -0,0 +1,108 @@
+// +build integration
+
+/*
+fsock_it_test.go is released under the MIT License <http://www.opensource.org/licenses/mit-license.php
+Copyright (C) ITsysCOM. All Rights Reserved.
+
+Provides FreeSWITCH socket communication.
+
+*/
+package fsock
+
+import (
+	"fmt"
+	"log/syslog"
+	"testing"
+	"time"
+)
+
+var FSTests = []func(*FSock, *testing.T){
+	testSendCmd,
+	testSendApiCmd,
+	testSendBgapiCmd1,
+	testReconect,
+	testSendCmd,
+	testSendApiCmd,
+	testSendBgapiCmd1,
+}
+
+func TestFSock(t *testing.T) {
+	faddr := "127.0.0.1:8021"
+	fpass := "ClueCon"
+	noreconects := 10
+	conID := "wetsfnmretiewrtpj"
+	l, errLog := syslog.New(syslog.LOG_INFO, "TestFSock")
+	if errLog != nil {
+		t.Fatal(errLog)
+	}
+	evFilters := make(map[string][]string)
+	evHandlers := make(map[string][]func(string, string))
+
+	fs, err := NewFSock(faddr, fpass, noreconects, evHandlers, evFilters, l, conID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !fs.Connected() {
+		t.Errorf("Coudn't connect to freeswitch!")
+	}
+
+	for _, ft := range FSTests {
+		t.Run("FSock", func(t *testing.T) { ft(fs, t) })
+	}
+
+	if err = fs.Disconnect(); err != nil {
+		t.Error(err)
+	}
+}
+
+func testReconect(fs *FSock, t *testing.T) {
+	if err := fs.Disconnect(); err != nil {
+		t.Error(err)
+	}
+
+	if err := fs.ReconnectIfNeeded(); err != nil {
+		t.Error(err)
+	}
+
+	if !fs.Connected() {
+		t.Errorf("Coudn't connect to freeswitch!")
+	}
+}
+
+func testSendCmd(fs *FSock, t *testing.T) {
+	expected := "Command recived!"
+	cmd := fmt.Sprintf("api eval %s\n\n", expected)
+	if rply, err := fs.SendCmd(cmd); err != nil {
+		t.Error(err)
+	} else if rply != expected {
+		t.Errorf("Expected: %s , recieved: %s", expected, rply)
+	}
+}
+
+func testSendApiCmd(fs *FSock, t *testing.T) {
+	expected := "Command recived!"
+	cmd := fmt.Sprintf("eval %s", expected)
+	if rply, err := fs.SendApiCmd(cmd); err != nil {
+		t.Error(err)
+	} else if rply != expected {
+		t.Errorf("Expected: %s , recieved: %s", expected, rply)
+	}
+}
+
+func testSendBgapiCmd1(fs *FSock, t *testing.T) {
+	expected := "Command recived!"
+	cmd := fmt.Sprintf("eval %s", expected)
+	if ch, err := fs.SendBgapiCmd(cmd); err != nil {
+		t.Error(err)
+	} else {
+		var rply string
+		select {
+		case rply = <-ch:
+			if rply != expected {
+				t.Errorf("Expected: %q , recieved: %q", expected, rply)
+			}
+		case <-time.After(5 * time.Second):
+			t.Errorf("Timeout")
+		}
+	}
+}
