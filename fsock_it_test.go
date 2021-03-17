f6d4d8b503aded50ba0aba2d521f931de724e531
ionutboangiu <ionut.boangiu@itsyscom.com>
2021-03-17 14:30:38 +0200

    Cover the remainder of fsock

--- fsock_it_test.go
+++ fsock_it_test.go
@@ -12,7 +12,9 @@ package fsock
 import (
 	"fmt"
 	"log/syslog"
+	"net"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 )
@@ -163,5 +165,109 @@ func TestFSockNewFSockNilLogger(t *testing.T) {
 
 	if fs != nil {
 		t.Errorf("\nExpected: <%+v>, \nReceived: <%+v>", nil, fs)
+	}
+}
+
+func TestFSockconnect(t *testing.T) {
+	const fsaddr = "127.0.0.1:8989"
+	fs := &FSock{
+		fsMutex:         &sync.RWMutex{},
+		fsaddress:       fsaddr,
+		fspaswd:         "pass",
+		eventHandlers:   make(map[string][]func(string, int)),
+		eventFilters:    make(map[string][]string),
+		backgroundChans: make(map[string]chan string),
+		cmdChan:         make(chan string),
+		reconnects:      -1,
+		delayFunc:       fib(),
+		logger:          nopLogger{},
+	}
+	l, err := net.Listen("tcp", fsaddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		conn.Write([]byte("not valid"))
+		conn.Close()
+	}()
+	experr1 := "Received error<EOF> when receiving the auth challenge"
+	if err := fs.connect(); err.Error() != experr1 {
+		t.Errorf("\nExpected: <%+v>, \nReceived: <%+v>", experr1, err)
+	}
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		conn.Write([]byte("not valid\n\n"))
+		conn.Close()
+	}()
+	experr2 := "No auth challenge received"
+	if err := fs.connect(); err.Error() != experr2 {
+		t.Errorf("\nExpected: <%+v>, \nReceived: <%+v>", experr2, err)
+	}
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		conn.Write([]byte("Content-Type: auth/request\n\n"))
+		c := make([]byte, 512)
+		conn.Read(c)
+		conn.Write([]byte("Content-Type: command/reply\nReply-Text:  accepted\n\n"))
+		// conn.Write([]byte("Content-Type: command/reply\nReply-Text: +OK accepted\n\n"))
+		conn.Close()
+	}()
+	experr3 := "Unexpected auth reply received: <Content-Type: command/reply\nReply-Text:  accepted\n>"
+	if err := fs.connect(); err.Error() != experr3 {
+		t.Errorf("\nExpected: <%+v>, \nReceived: <%+v>", experr3, err)
+	}
+	fs.eventFilters["Event-Name"] = []string{"CUSTOM"}
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		conn.Write([]byte("Content-Type: auth/request\n\n"))
+		c := make([]byte, 512)
+		conn.Read(c)
+		conn.Write([]byte("Content-Type: command/reply\nReply-Text: +OK accepted\n\n"))
+		c = make([]byte, 512)
+		conn.Read(c)
+		conn.Write([]byte("not valid"))
+		conn.Close()
+	}()
+	experr4 := "EOF"
+	if err := fs.connect(); err.Error() != experr4 {
+		t.Errorf("\nExpected: <%+v>, \nReceived: <%+v>", experr4, err)
+	}
+	fs.eventHandlers["ALL"] = nil
+	fs.eventFilters = make(map[string][]string)
+	go func() {
+		conn, err := l.Accept()
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		conn.Write([]byte("Content-Type: auth/request\n\n"))
+		c := make([]byte, 512)
+		conn.Read(c)
+		conn.Write([]byte("Content-Type: command/reply\nReply-Text: +OK accepted\n\n"))
+		c = make([]byte, 512)
+		conn.Read(c)
+		conn.Write([]byte("not valid"))
+		conn.Close()
+	}()
+	experr5 := "EOF"
+	if err := fs.connect(); err.Error() != experr5 {
+		t.Errorf("\nExpected: <%+v>, \nReceived: <%+v>", experr5, err)
 	}
 }
